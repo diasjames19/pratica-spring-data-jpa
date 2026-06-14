581abfee59610144a34e745450dfed80cd747d3d
diasjames19 <diasjames@hotmail.com>
2026-06-14 17:29:37 -0300

    Projeto

---
+++ src/main/java/com/james/pratica_jpa/PraticaJpaApplication.java
@@ -0,0 +1,13 @@
+package com.james.pratica_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraticaJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraticaJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/james/pratica_jpa/PraticaJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.james.pratica_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraticaJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
