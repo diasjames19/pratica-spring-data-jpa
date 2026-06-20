1ee5b3d296e3c28884fbc76fb1d84ee4e961fa7b
diasjames19 <diasjames@hotmail.com>
2026-06-19 23:08:23 -0300

    Cirando entidade avalicao fisica

--- src/main/java/com/james/pratica_jpa/Entity/PhysicalAssessment.java
+++ src/main/java/com/james/pratica_jpa/Entity/PhysicalAssessment.java
@@ -1,5 +1,32 @@
 package com.james.pratica_jpa.Entity;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_physical_assessment")
 public class PhysicalAssessment {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id")
+    private Student student;
+
+    private LocalDateTime Evaluationdate = LocalDateTime.now();
 }
