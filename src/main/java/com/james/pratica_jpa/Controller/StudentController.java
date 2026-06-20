b7fbb28e5d27601d03ca04e00da97f911d8ad836
diasjames19 <diasjames@hotmail.com>
2026-06-19 22:41:00 -0300

    Service e Controller

--- src/main/java/com/james/pratica_jpa/Controller/StudentController.java
+++ src/main/java/com/james/pratica_jpa/Controller/StudentController.java
@@ -1,8 +1,26 @@
 package com.james.pratica_jpa.Controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.james.pratica_jpa.Entity.Student;
+import com.james.pratica_jpa.Entity.Dto.StudentDto;
+import com.james.pratica_jpa.Service.StudentService;
+
+import jakarta.validation.Valid;
+
 @RestController
+@RequestMapping("/student")
 public class StudentController {
+
+private StudentService service;
+
+@PostMapping
+public Student Create(@Valid @RequestBody StudentDto dto) {
+      
+        return service.create(dto);
+    }
     
 }
--- src/main/java/com/james/pratica_jpa/Service/StudentService.java
+++ src/main/java/com/james/pratica_jpa/Service/StudentService.java
@@ -7,15 +7,24 @@ import org.springframework.stereotype.Service;
 import com.james.pratica_jpa.Entity.PhysicalAssessment;
 import com.james.pratica_jpa.Entity.Student;
 import com.james.pratica_jpa.Entity.Dto.StudentDto;
+import com.james.pratica_jpa.Repository.StudentRepository;
 import com.james.pratica_jpa.Service.Interface.IStudentService;
 
 @Service
 public class StudentService implements IStudentService{
 
+    private StudentRepository repository;
+
     @Override
     public Student create(StudentDto dto) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'create'");
+        Student newStudent = new Student();
+        newStudent.setName(dto.getName());
+        newStudent.setCpf(dto.getCpf());
+        newStudent.setNeighborhood(dto.getNeighborhood());
+        newStudent.setDateOfBirth(dto.getDateOfBirth());
+
+        return repository.save(newStudent);
+        
     }
 
     @Override
